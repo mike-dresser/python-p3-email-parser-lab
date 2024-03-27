17c4df111e32b7f902c2dacd66b77f654325603c
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-27 13:16:54 -0400

    completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,20 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    
+    def __init__(self, string) -> None:
+        self.string = string 
+
+    def parse(self):
+        pattern = r'[\s,]+'
+        split = re.split(pattern, self.string)
+        print(f'split: {split}')
+        output = []
+        for item in split:
+            pattern = re.compile(r'[A-z0-9\.+-]+@[A-z0-9]+\.[A-z]+')
+            print(f'item: {item}, match: {pattern.match(item)}')
+            if pattern.match(item):
+                output.append(item)
+        unique = list(set(output))
+        unique.sort()
+        return unique
